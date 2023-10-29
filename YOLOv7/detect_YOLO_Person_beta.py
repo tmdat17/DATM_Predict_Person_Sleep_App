bc858455ef78ac25669ac50a778b849fc397193d
Dat17 <tmdatsc.vl@gmail.com>
2023-10-29 15:05:55 +0700

    add folder YOLOv7

---
+++ YOLOv7/detect_YOLO_Person_beta.py
@@ -0,0 +1,102 @@
+import cv2
+import os
+from yolov7_detector import initialize_model, detect_objects
+
+
+# cap = cv2.VideoCapture("192.168.1.10:4747")
+weights_path = "E:\CTU\LUAN_VAN_2023\YOLOv7\pretrain\yolov7.pt"
+
+
+# --------------------------------------------- Static Image
+# img_path = 'E:\CTU\LUAN_VAN_2023\sample_img_pose\pose_sit_wi_1_resize.jpg'
+
+# img = cv2.imread(img_path)
+# model, device, half= initialize_model(weights_path)
+# x, y, w, h = detect_objects(model, device, img, half, img_size=640, conf_thres=0.1, iou_thres=0.45)
+# # x, y, w, h = detect_objects(model, device, img_path, half, img_size=640, conf_thres=0.1, iou_thres=0.45)
+# print('TEST:    ', x,y,w,h)
+# cv2.rectangle(img, (x, y), (x+w, y+h), color = (0, 0, 255), thickness=2)
+# cv2.imshow('Image with Bounding Box', img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+# --------------------------------------------- Video
+# HEIGHT = 128
+# WIDTH = 128
+index = 0
+# action = 'stand'
+# cap = cv2.VideoCapture('../Data_Train_Pose/full_{}.mov'.format(action))
+cap = cv2.VideoCapture('../converted_mp4_code/at_home/full_lie_sleep_at_home.mp4')
+model, device, half= initialize_model(weights_path)
+fast_forward_rate = 15
+while True:
+    # Đặt vị trí hiện tại của video để tua nhanh
+    cap.set(cv2.CAP_PROP_POS_FRAMES, cap.get(cv2.CAP_PROP_POS_FRAMES) + fast_forward_rate)
+    ret, frame = cap.read()
+
+    if ret:
+        height, width = frame.shape[:2]
+        for i in range(5):
+            if i==4:
+                width = int(width * 0.99)
+                height = int(height * 0.99)
+            else:
+                width = int(width * 0.8)
+                height = int(height * 0.8)
+
+        # Resize ảnh
+        resized_img = cv2.resize(frame, (width, height))
+
+        x, y, w, h, confidence_formatted = detect_objects(model, device, resized_img, half, img_size=640, conf_thres=0.1, iou_thres=0.45)
+
+        roi = resized_img[y:y+h,x:x+w]
+        
+        # if roi is not None and roi.size != 0:
+        #     print("prepare excute image {}:".format(index))
+        #     # roi_gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+        #     # roi_gray = cv2.resize(roi_gray,(WIDTH,HEIGHT),interpolation=cv2.INTER_AREA)
+        #     if not os.path.exists('../data_cropped/'):
+        #         os.makedirs('../data_cropped/')
+        #     if action == 'stand':
+        #         if not os.path.exists('../data_cropped/stand'):
+        #             os.makedirs('../data_cropped/stand')
+        #         name_img = '{}_{}.jpg'.format(action, index)
+        #         filename="../data_cropped/{}/".format(action)+str(name_img)
+        #         # cv2.imwrite(filename,roi_gray)
+        #         cv2.imwrite(filename,roi)
+        #         print("saved image {}:".format(index))
+        #         print("path: {}".format(filename))
+        #         index += 1
+        #     elif action == 'sit':
+        #         if not os.path.exists('../data_cropped/sit'):
+        #             os.makedirs('./data_cropped/sit')
+        #         name_img = '{}_{}.jpg'.format(action, index)
+        #         filename="../data_cropped/{}/".format(action)+str(name_img)
+        #         # cv2.imwrite(filename,roi_gray)
+        #         cv2.imwrite(filename,roi)
+        #         print("saved image {}:".format(index))
+        #         index += 1
+        #     else:
+        #         if not os.path.exists('../data_cropped/lie'):
+        #             os.makedirs('../data_cropped/lie')
+        #         name_img = '{}_{}.jpg'.format(action, index)
+        #         filename="../data_cropped/{}/".format(action)+str(name_img)
+        #         # cv2.imwrite(filename,roi_gray)
+        #         cv2.imwrite(filename,roi)
+        #         print("saved image {}:".format(index))
+        #         index += 1
+        # else:
+        #     print('Error: Invalid image!')
+        
+        cv2.rectangle(resized_img, (x, y), (x+w, y+h), color = (0, 0, 255), thickness=2)
+        cv2.putText(resized_img, str(confidence_formatted) + "% YOLO", (x+60, y-90), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 100, 255), 2, cv2.LINE_AA)
+        cv2.imshow('Video', resized_img)
+
+        # Bấm 'q' để thoát
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ YOLOv7/detect_person_package.py
@@ -0,0 +1,83 @@
+import cv2
+import torch
+import numpy as np
+from numpy import random
+from models.experimental import attempt_load
+from utils.general import check_img_size, non_max_suppression, scale_coords, set_logging
+from utils.plots import plot_one_box
+from utils.torch_utils import select_device, time_synchronized
+
+# Sử dụng class YOLOv7Detector
+weights_path = "E:\CTU\LUAN_VAN_2023\YOLOv7\pretrain\yolov7.pt"
+source_image_path = "E:\CTU\LUAN_VAN_2023\sample_img_pose\pose_sit_wi_1_resize.jpg"
+
+
+class YOLOv7Detector:
+    def __init__(self, weights, img_size=640, conf_thres=0.1, iou_thres=0.45, device='cpu', classes=None):
+        self.weights = weights
+        self.img_size = img_size
+        self.conf_thres = conf_thres
+        self.iou_thres = iou_thres
+        self.device = device
+        self.classes = classes
+        
+        self.model = None
+        self.names = None
+        self.colors = None
+
+    def initialize_model(self):
+        set_logging()
+        device = select_device(self.device)
+        self.model = attempt_load(self.weights, map_location=device)
+        stride = int(self.model.stride.max())
+        self.img_size = check_img_size(self.img_size, s=stride)
+        if device.type != 'cpu':
+            self.model(torch.zeros(1, 3, self.img_size, self.img_size).to(device).type_as(next(self.model.parameters())))
+
+        self.names = self.model.module.names if hasattr(self.model, 'module') else self.model.names
+        self.colors = [[random.randint(0, 255) for _ in range(3)] for _ in self.names]
+
+    def detect_objects(self, source_image_path):
+        img0 = cv2.imread(source_image_path)
+        img = letterbox(img0, self.img_size, stride=self.model.stride.max())[0]
+        img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3xHxW
+        img = np.ascontiguousarray(img)
+        img = torch.from_numpy(img).to(self.device)
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+
+        # Inference
+        pred = self.model(img, augment=False)[0]
+
+        # Apply NMS
+        classes = None
+        if self.classes:
+            classes = []
+            for class_name in self.classes:
+                classes.append(self.classes.index(class_name))
+
+        pred = non_max_suppression(pred, self.conf_thres, self.iou_thres, classes=classes, agnostic=False)
+        for i, det in enumerate(pred):
+            if len(det):
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], img0.shape).round()
+
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    label = f"{n} {self.names[int(c)]}{'s' * (n > 1)}"
+                    for *xyxy, conf, cls in reversed(det):
+                        x_min, y_min, x_max, y_max = map(int, xyxy)  # Lấy giá trị tọa độ x_min, y_min, x_max, y_max
+                        width = x_max - x_min  # Tính toán chiều rộng
+                        height = y_max - y_min  # Tính toán chiều dài
+                        print('---------------------------------------------------------------------')
+                        print('x, y, w, h:', x_min, y_min, width, height)
+                        label = f'{self.names[int(cls)]} {conf:.2f}'
+                        plot_one_box(xyxy, img0, label=label, color=self.colors[int(cls)], line_thickness=3)
+
+        cv2.imshow('predict YOLOv7', img0)
+        cv2.waitKey(0)
+        
+# Đoạn này sẽ được gọi bên file khác với 3 dòng bên dưới
+detector = YOLOv7Detector(weights_path, classes=['person'])
+detector.initialize_model()
+detector.detect_objects(source_image_path)
